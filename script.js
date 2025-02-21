74dbb8982866b70bbe43b1d885b7154acbe37966
tnpquyen1117 <tnpquyen110107@gmail.com>
2025-02-21 14:07:11 +0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login()
+{
+  let u = username.value
+  let p = pw.value
+  if (u=="hieu" && p=="123")
+  {
+    ưindow.open("home.html")
+    document.getElementById(compiler).style.display="block"
+  }
+}
